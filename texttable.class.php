2620224f848fe0437f6cc4b7f399ebcc65ad404c
danda <danda@osc.co.cr>
2018-07-12 02:07:36 +0800

    use static:: instead of self:: in case someone needs to inherit a method

--- texttable.class.php
+++ texttable.class.php
@@ -34,51 +34,61 @@ class texttable {
 
         $header = $footer = null;
         if( $headertype == 'keys' ) {        
-            $header = array_keys( self::obj_arr( $rows[0] ) );
+            $header = array_keys( static::obj_arr( $rows[0] ) );
         }
         else if( $headertype == 'firstrow' ) {
-            $header = self::obj_arr( array_shift( $rows ) );
+            $header = static::obj_arr( array_shift( $rows ) );
         }
         if( $footertype == 'keys' && count( $rows ) ) {
-            $footer = array_keys( self::obj_arr( $rows[count($rows) - 1] ) );
+            $footer = array_keys( static::obj_arr( $rows[count($rows) - 1] ) );
         }
         else if( $footertype == 'lastrow' && count( $rows ) ) {
-            $footer = self::obj_arr( array_pop( $rows ) );
+            $footer = static::obj_arr( array_pop( $rows ) );
         }
         
         $col_widths = array();
         
         if( $header ) {
-            self::calc_row_col_widths( $col_widths, $header );
+            static::calc_row_col_widths( $col_widths, $header );
         }
         if( $footer ) {
-            self::calc_row_col_widths( $col_widths, $footer );
+            static::calc_row_col_widths( $col_widths, $footer );
         }
         foreach( $rows as $row ) {
-            $row = self::obj_arr( $row );
-            self::calc_row_col_widths( $col_widths, $row );
+            $row = static::obj_arr( $row );
+            static::calc_row_col_widths( $col_widths, $row );
         }
         
         $buf = '';
         if( $header ) {        
-            $buf .= self::print_divider_row( $col_widths );
-            $buf .= self::print_row( $col_widths, $header );
+            $buf .= static::print_header($col_widths, $header );
         }
-        $buf .= self::print_divider_row( $col_widths );
+        $buf .= static::print_divider_row( $col_widths, 'top' );
         foreach( $rows as $row ) {
-            $row = self::obj_arr( $row );
-            $buf .= self::print_row( $col_widths, $row );
+            $row = static::obj_arr( $row );
+            $buf .= static::print_row( $col_widths, $row );
         }
-        $buf .= self::print_divider_row( $col_widths );
-        if( $footer ) {        
-            $buf .= self::print_row( $col_widths, $footer );
-            $buf .= self::print_divider_row( $col_widths );
+        $buf .= static::print_divider_row( $col_widths, 'bottom' );
+        if( $footer ) {
+            $buf .= static::print_footer($col_widths, $footer );
         }
         
         return $buf;
     }
     
-    static protected function print_divider_row( $col_widths ) {
+    static protected function print_footer($col_widths, $footer) {
+        $buf  = static::print_row( $col_widths, $footer );
+        $buf .= static::print_divider_row( $col_widths, 'footer' );
+        return $buf;
+    }
+
+    static protected function print_header($col_widths, $header) {
+        $buf  = static::print_row( $col_widths, $header );
+        $buf .= static::print_divider_row( $col_widths, 'header' );
+        return $buf;
+    }
+
+    static protected function print_divider_row( $col_widths, $position ) {
         $buf = '+';
         foreach( $col_widths as $width ) {
             $buf .= '-' . str_pad( '-', $width, '-' ) . "-+";
